1547b2efcccb82a28c9f20db2f87c1b1fdf87f33
clakr <clarktolosa@gmail.com>
2023-05-11 08:00:02 +0800

    resolved todos

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-/**
- * TODO:
- *  - change meta thru .env
- */
+(function () {
+  const title = document.querySelector("title");
+  if (!title) return;
+
+  title.textContent = import.meta.env.VITE_PROJECT_NAME;
+})();
